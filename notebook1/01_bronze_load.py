03243449a692314367666a2756a8c05bde47d7ae
lin.wang4@wipro.com <lin.wang4@wipro.com>
2026-08-24 19:22:20 +0000

    the 1. Version script

--- notebook1/01_bronze_load.py
+++ notebook1/01_bronze_load.py
@@ -1,1 +1,33 @@
-'test'
+# ============================
+# Bronze Customer Load
+# ============================
+
+from pyspark.sql.functions import current_timestamp, lit
+
+# 1. Read CSV
+customer_df = (
+    spark.read
+         .option("header", True)
+         .option("inferSchema", True)
+         .csv("dbfs:/FileStore/tables/customer.csv")
+)
+
+# 2. Add Bronze Metadata
+bronze_customer_df = (
+    customer_df
+    .withColumn("LoadTimestamp", current_timestamp())
+    .withColumn("SourceSystem", lit("SAP"))
+)
+
+# 3. Check Data
+display(bronze_customer_df)
+
+# 4. Create Bronze Delta Table
+(
+    bronze_customer_df.write
+    .format("delta")
+    .mode("overwrite")
+    .saveAsTable("bronze_customer")
+)
+
+print("Bronze Customer Table Created Successfully")
